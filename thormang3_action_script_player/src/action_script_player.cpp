5f5ca7aef5c12e853ad80b7780aaab963f034006
Jay-Song <hjsong@robotis.com>
2016-06-02 10:02:35 +0900

    Change name of all contens in code.

--- thormang3_action_script_player/src/action_script_player.cpp
+++ thormang3_action_script_player/src/action_script_player.cpp
@@ -1,5 +1,5 @@
 /*
- * thormang3_motion_script_player.cpp
+ * thormang3_action_script_player.cpp
  *
  *  Created on: Mar 25, 2016
  *      Author: jay
@@ -14,33 +14,33 @@
 #include "robotis_controller_msgs/StatusMsg.h"
 #include "thormang3_action_module_msgs/IsRunning.h"
 
-#define MOTION_PLAY_CMD_NAME  "play"
-#define MP3_PLAY_CMD_NAME     "mp3"
-#define WAIT_MOTION_CMD_NAME  "wait"
-#define SLEEP_CMD_NAME        "sleep"
+#define ACTION_PLAY_CMD_NAME              "play"
+#define MP3_PLAY_CMD_NAME                 "mp3"
+#define WAIT_ACTION_PLAY_FINISH_CMD_NAME  "wait"
+#define SLEEP_CMD_NAME                    "sleep"
 
 
 
-ros::Subscriber motion_script_num_sub;
-ros::Publisher  action_page_num_pub;
-ros::Publisher  sound_file_name_pub;
+ros::Subscriber    action_script_num_sub;
+ros::Publisher     action_page_num_pub;
+ros::Publisher     sound_file_name_pub;
 ros::ServiceClient is_running_client;
 
 thormang3_action_module_msgs::IsRunning  is_running_srv;
 
-boost::thread   *motion_script_play_thread;
+boost::thread   *action_script_play_thread;
 
 
-std::string motion_script_file_path;
+std::string action_script_file_path;
 
 typedef struct
 {
     std::string cmd_name;
     std::string cmd_arg_str;
     int         cmd_arg_int;
-} motion_script_cmd;
+} action_script_cmd;
 
-std::vector<motion_script_cmd> motion_script_data;
+std::vector<action_script_cmd> action_script_data;
 
 std::string convertIntToString(int n)
 {
@@ -72,29 +72,29 @@ bool IsActionRunning(void)
 }
 
 
-bool parseMotionScript(int motion_script_index)
+bool parseActionScript(int action_script_index)
 {
-    motion_script_data.clear();
+    action_script_data.clear();
 
-    YAML::Node motion_script_file_doc;
+    YAML::Node action_script_file_doc;
     try
     {
         // load yaml
-        motion_script_file_doc = YAML::LoadFile(motion_script_file_path.c_str());
+        action_script_file_doc = YAML::LoadFile(action_script_file_path.c_str());
     }
     catch(const std::exception& e)
     {
-        ROS_ERROR("Failed to load motion script file.");
+        ROS_ERROR("Failed to load action script file.");
         return false;
     }
 
 
-    //find motion script
-    std::string script_index_key = "script" + convertIntToString(motion_script_index);
-    YAML::Node motion_script_doc = motion_script_file_doc[script_index_key];
-    if(motion_script_doc == NULL)
+    //find action script
+    std::string script_index_key = "script" + convertIntToString(action_script_index);
+    YAML::Node  action_script_doc = action_script_file_doc[script_index_key];
+    if(action_script_doc == NULL)
     {
-        std::string status_msg = "Failed to find motion script #" + convertIntToString(motion_script_index);
+        std::string status_msg = "Failed to find action script #" + convertIntToString(action_script_index);
         ROS_ERROR_STREAM(status_msg);
         return false;
     }
@@ -108,116 +108,116 @@ bool parseMotionScript(int motion_script_index)
     	{
     		//check cmd exist
     		cmd_key = "cmd" + convertIntToString(cmd_num);
-    		YAML::Node motion_script_cmd_doc = motion_script_doc[cmd_key];
-    		if(motion_script_cmd_doc == NULL)
+    		YAML::Node action_script_cmd_doc = action_script_doc[cmd_key];
+    		if(action_script_cmd_doc == NULL)
     		{
     			break;
     		}
 
     		//check validity of cmd_name
-    		motion_script_cmd temp_cmd;
-    		if(motion_script_cmd_doc["cmd_name"] == NULL)
+    		action_script_cmd temp_cmd;
+    		if(action_script_cmd_doc["cmd_name"] == NULL)
     		{
-    			std::string status_msg = cmd_key + " of " + "script" + convertIntToString(motion_script_index) + " is invalid.";
+    			std::string status_msg = "cmd#" + convertIntToString(cmd_num) + " of " + "script#" + convertIntToString(action_script_index) + " is invalid.";
     			ROS_ERROR_STREAM(status_msg);
     			return false;
     		}
 
     		//check  validity of cmd_arg
-    		temp_cmd.cmd_name = motion_script_cmd_doc["cmd_name"].as<std::string>();
-    		if((temp_cmd.cmd_name != "wait") && (motion_script_cmd_doc["cmd_arg"] == NULL))
+    		temp_cmd.cmd_name = action_script_cmd_doc["cmd_name"].as<std::string>();
+    		if((temp_cmd.cmd_name != "wait") && (action_script_cmd_doc["cmd_arg"] == NULL))
     		{
-    			std::string status_msg = cmd_key + " of " + "script" + convertIntToString(motion_script_index) + " is invalid.";
+    			std::string status_msg = "cmd#" + convertIntToString(cmd_num) + " of " + "script#" + convertIntToString(action_script_index) + " is invalid.";
     			ROS_ERROR_STREAM(status_msg);
     			return false;
     		}
 
     		//get cmd_arg
-    		if(temp_cmd.cmd_name == MOTION_PLAY_CMD_NAME)
+    		if(temp_cmd.cmd_name == ACTION_PLAY_CMD_NAME)
     		{
-    			temp_cmd.cmd_arg_int = motion_script_cmd_doc["cmd_arg"].as<int>();
+    			temp_cmd.cmd_arg_int = action_script_cmd_doc["cmd_arg"].as<int>();
     		}
     		else if(temp_cmd.cmd_name == MP3_PLAY_CMD_NAME)
     		{
-    			temp_cmd.cmd_arg_str = motion_script_cmd_doc["cmd_arg"].as<std::string>();
+    			temp_cmd.cmd_arg_str = action_script_cmd_doc["cmd_arg"].as<std::string>();
     		}
-    		else if(temp_cmd.cmd_name == WAIT_MOTION_CMD_NAME)
+    		else if(temp_cmd.cmd_name == WAIT_ACTION_PLAY_FINISH_CMD_NAME)
     		{
     			temp_cmd.cmd_arg_str = "";
     			temp_cmd.cmd_arg_int = 0;
     		}
     		else if(temp_cmd.cmd_name == SLEEP_CMD_NAME)
     		{
-    			temp_cmd.cmd_arg_int = motion_script_cmd_doc["cmd_arg"].as<int>();
+    			temp_cmd.cmd_arg_int = action_script_cmd_doc["cmd_arg"].as<int>();
     			if(temp_cmd.cmd_arg_int < 0 )
     			{
-        			std::string status_msg = cmd_key + " of " + "script" + convertIntToString(motion_script_index) + " is invalid.";
+        			std::string status_msg = "cmd#" + convertIntToString(cmd_num) + " of " + "script#" + convertIntToString(action_script_index) + " is invalid.";
         			ROS_ERROR_STREAM(status_msg);
-        			motion_script_data.clear();
+        			action_script_data.clear();
         			return false;
     			}
     		}
     		else
     		{
-    			std::string status_msg = cmd_key + " of " + "script" + convertIntToString(motion_script_index) + " is invalid.";
+    			std::string status_msg = "cmd#" + convertIntToString(cmd_num) + " of " + "script#" + convertIntToString(action_script_index) + " is invalid.";
     			ROS_ERROR_STREAM(status_msg);
-    			motion_script_data.clear();
+    			action_script_data.clear();
     			return false;
     		}
 
-    		motion_script_data.push_back(temp_cmd);
+    		action_script_data.push_back(temp_cmd);
     		cmd_num++;
     	}
     }
     catch(const std::exception& e)
     {
-		std::string status_msg = cmd_key + " of " + "script" + convertIntToString(motion_script_index) + " is invalid.";
+		std::string status_msg = "cmd#" + convertIntToString(cmd_num) + " of " + "script#" + convertIntToString(action_script_index) + " is invalid.";
 		ROS_ERROR_STREAM(status_msg);
-		motion_script_data.clear();
+		action_script_data.clear();
     	return false;
     }
 
     return true;
 }
 
-void motionScriptPlayThreadFunc(int motion_script_index)
+void actionScriptPlayThreadFunc(int action_script_index)
 {
     try
     {
-        if(motion_script_index < 0)
+        if(action_script_index < 0)
         {
-            std::string status_msg = "Invalid Motion Script Index";
+            std::string status_msg = "Invalid Action Script Index";
             ROS_ERROR_STREAM(status_msg);
             return;
         }
 
         if(IsActionRunning() == true)
         {
-            std::string status_msg = "Previous motion playing is not finished.";
+            std::string status_msg = "Previous action playing is not finished.";
             ROS_ERROR_STREAM(status_msg);
             return;
         }
 
-        if(parseMotionScript(motion_script_index) == false)
+        if(parseActionScript(action_script_index) == false)
             return;
 
-        for(unsigned int motion_script_data_idx = 0; motion_script_data_idx < motion_script_data.size(); motion_script_data_idx++)
+        for(unsigned int action_script_data_idx = 0; action_script_data_idx < action_script_data.size(); action_script_data_idx++)
         {
-            std::string cmd_name = motion_script_data[motion_script_data_idx].cmd_name;
+            std::string cmd_name = action_script_data[action_script_data_idx].cmd_name;
             std_msgs::Int32   action_page_num_msg;
             std_msgs::String  sound_file_name_msg;
 
-            if(cmd_name == MOTION_PLAY_CMD_NAME)
+            if(cmd_name == ACTION_PLAY_CMD_NAME)
             {
-                action_page_num_msg.data = motion_script_data[motion_script_data_idx].cmd_arg_int;
+                action_page_num_msg.data = action_script_data[action_script_data_idx].cmd_arg_int;
                 action_page_num_pub.publish(action_page_num_msg);
             }
             else if(cmd_name == MP3_PLAY_CMD_NAME)
             {
-                sound_file_name_msg.data = motion_script_data[motion_script_data_idx].cmd_arg_str;
+                sound_file_name_msg.data = action_script_data[action_script_data_idx].cmd_arg_str;
                 sound_file_name_pub.publish(sound_file_name_msg);
             }
-            else if(cmd_name == WAIT_MOTION_CMD_NAME)
+            else if(cmd_name == WAIT_ACTION_PLAY_FINISH_CMD_NAME)
             {
                 while(true)
                 {
@@ -229,7 +229,7 @@ void motionScriptPlayThreadFunc(int motion_script_index)
             }
             else if(cmd_name == SLEEP_CMD_NAME)
             {
-                boost::this_thread::sleep(boost::posix_time::milliseconds(motion_script_data[motion_script_data_idx].cmd_arg_int));
+                boost::this_thread::sleep(boost::posix_time::milliseconds(action_script_data[action_script_data_idx].cmd_arg_int));
             }
             else
             {
@@ -242,12 +242,12 @@ void motionScriptPlayThreadFunc(int motion_script_index)
     }
     catch (boost::thread_interrupted&)
     {
-        ROS_INFO("Motion Script Thread is Interrupted");
+        ROS_INFO("Action Script Thread is Interrupted");
         return;
     }
 }
 
-void motionScriptNumberCallback(const std_msgs::Int32::ConstPtr& msg)
+void actionScriptNumberCallback(const std_msgs::Int32::ConstPtr& msg)
 {
     if((msg->data == -1) || (msg->data == -2)) //Stop or Break
     {
@@ -255,33 +255,33 @@ void motionScriptNumberCallback(const std_msgs::Int32::ConstPtr& msg)
         action_page_num_msg.data = msg->data;
         action_page_num_pub.publish(action_page_num_msg);
 
-        if((motion_script_play_thread != 0)	&& (motion_script_play_thread->get_thread_info() != 0))
+        if((action_script_play_thread != 0)	&& (action_script_play_thread->get_thread_info() != 0))
         {
-        	if(motion_script_play_thread->get_thread_info()->done == false)
+        	if(action_script_play_thread->get_thread_info()->done == false)
         	{
-        		motion_script_play_thread->interrupt();
-        		motion_script_play_thread->join();
+        		action_script_play_thread->interrupt();
+        		action_script_play_thread->join();
         	}
         }
     }
     else
     {
-        if((motion_script_play_thread == 0))
+        if((action_script_play_thread == 0))
         {
-            motion_script_play_thread = new boost::thread(motionScriptPlayThreadFunc, msg->data);
+            action_script_play_thread = new boost::thread(actionScriptPlayThreadFunc, msg->data);
         }
-        else if(motion_script_play_thread->get_thread_info() == 0)
+        else if(action_script_play_thread->get_thread_info() == 0)
         {
-        	motion_script_play_thread = new boost::thread(motionScriptPlayThreadFunc, msg->data);
+        	action_script_play_thread = new boost::thread(actionScriptPlayThreadFunc, msg->data);
         }
-        else if(motion_script_play_thread->get_thread_info()->done == true)
+        else if(action_script_play_thread->get_thread_info()->done == true)
         {
-            delete motion_script_play_thread;
-            motion_script_play_thread = new boost::thread(motionScriptPlayThreadFunc, msg->data);
+            delete action_script_play_thread;
+            action_script_play_thread = new boost::thread(actionScriptPlayThreadFunc, msg->data);
         }
         else
         {
-            std::string status_msg = "Previous motion script is not finished.";
+            std::string status_msg = "Previous action script is not finished.";
             ROS_ERROR_STREAM(status_msg);
         }
     }
@@ -289,26 +289,26 @@ void motionScriptNumberCallback(const std_msgs::Int32::ConstPtr& msg)
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "THORMANG3_MOTION_SCRIPT_PLAYER");
+    ros::init(argc, argv, "THORMANG3_ACTION_SCRIPT_PLAYER");
     ros::NodeHandle ros_node_handle;
 
-    motion_script_play_thread = 0;
+    action_script_play_thread = 0;
 
-    motion_script_num_sub = ros_node_handle.subscribe("/robotis/demo/motion_index", 1, &motionScriptNumberCallback);
+    action_script_num_sub = ros_node_handle.subscribe("/robotis/demo/action_index", 1, &actionScriptNumberCallback);
     action_page_num_pub   = ros_node_handle.advertise<std_msgs::Int32>("/robotis/action/page_num", 0);
     sound_file_name_pub   = ros_node_handle.advertise<std_msgs::String>("/play_sound_file", 0);
     is_running_client     = ros_node_handle.serviceClient<thormang3_action_module_msgs::IsRunning>("/robotis/action/is_running");
 
-    //Setting motion script file path
-    std::string temp_motion_script_file_path = ros::package::getPath("thormang3_motion_script_player") + "/script/motion_script.yaml";
-    if(ros_node_handle.getParam("motion_script_file_path", motion_script_file_path) == false)
+    //Setting action script file path
+    std::string temp_action_script_file_path = ros::package::getPath("thormang3_action_script_player") + "/script/action_script.yaml";
+    if(ros_node_handle.getParam("action_script_file_path", action_script_file_path) == false)
     {
-        motion_script_file_path = temp_motion_script_file_path;
-        ROS_WARN("Failed to get motion script_file_path.");
-        ROS_WARN("The default motion script file path will be used.");
+        action_script_file_path = temp_action_script_file_path;
+        ROS_WARN("Failed to get action script_file_path.");
+        ROS_WARN("The default action script file path will be used.");
     }
 
-    ROS_INFO("Start ThorMang3 Motion Script Player");
+    ROS_INFO("Start ThorMang3 Action Script Player");
 
     ros::spin();
 }
